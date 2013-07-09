04c365c6a46840f7e5c9625eaef8719851557c9e
Roger Vallès <roger.valles@socialpoint.es>
2013-07-09 20:05:24 +0200

    fixed buffer ini size

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-buf = new Buffer(4096);
 content = fs.readFileSync('index.html', 'utf-8');
+buf = new Buffer(content.length);
 buf.write(content, 'utf-8');
-//console.log(buf.toString('utf-8'));
+
 app.get('/', function(request, response) {
    response.send(buf.toString('utf-8'));
 });
@@ -15,4 +15,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-//console.log(buf.toString('utf-8'));

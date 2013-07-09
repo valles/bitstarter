b49f5dfa60f2df74efc2838125b424db8f7f03ca
Roger Vallès <roger.valles@socialpoint.es>
2013-07-09 21:07:42 +0200

    added check from URL

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://agile-chamber-2857.herokuapp.com/"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +36,16 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+var urlExists = function(url){
+    rest.get(url).on('complete', function(result) {
+  if (result instanceof Error) {
+    console.log('Error: ' + result.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    return result;
+  }
+});
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,6 +77,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <check_url>', 'URL', clone(urlExists), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);

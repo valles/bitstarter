47061f74cf6c7955d22f411447588fb934e2e831
Roger Vallès <roger.valles@socialpoint.es>
2013-07-09 19:37:56 +0200

    set buffer size to 4096

--- web.js
+++ web.js
@@ -3,15 +3,16 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-buf = new Buffer(256);
+buf = new Buffer(4096);
 content = fs.readFileSync('index.html', 'utf-8');
 buf.write(content, 'utf-8');
 //console.log(buf.toString('utf-8'));
 app.get('/', function(request, response) {
-   response.send(buf.toString('utf-8', 0, 27));
+   response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+//console.log(buf.toString('utf-8'));
